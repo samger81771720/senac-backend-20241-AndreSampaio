027beeb6bf73c1febc5c89737eb5db63c0c6451d
sam_g <sam_g@Andr>
2024-03-16 10:25:53 -0300

    Inclusão de registros para testar o scrpit do banco de dados.

--- src/main/resources/scripts/create_tables_vacinacao.sql
+++ src/main/resources/scripts/create_tables_vacinacao.sql
@@ -1,3 +1,5 @@
+drop database if exists vacinacao;
+
 create schema if not exists VACINACAO;
 
 use VACINACAO;
@@ -33,3 +35,33 @@ create table if not exists VACINACAO.APLICACAO_VACINA(
 	constraint PESSOA_fk foreign key(id_Pessoa) references VACINACAO.PESSOA(id_Pessoa),
 	constraint VACINA_fk foreign key(id_Vacina) references VACINACAO.VACINA(id_Vacina) 
 );
+
+
+INSERT INTO VACINACAO.PESSOA (tipo, nome, dataNascimento, sexo, cpf) VALUES 
+(2, 'Carlos Eduardo Silveira', '1967-11-13', 'M', '16391459401'),
+(1, 'Maria Clara da Cunha', '1980-03-27', 'F', '16391459402'),
+(3, 'Kaique da Conceição', '1966-09-11', 'M', '16391459410'),
+(1, 'Josué Silveira', '1980-11-07','M', '05469852365'),
+(1, 'João Siqueira', '1969-11-01','M','56985632012'),
+(2, 'Joana Silva', '1970-05-15', 'F', '16391459403'),
+(1, 'Roberto Alves', '1985-07-30', 'M', '16391459404'),
+(3, 'Ana Maria', '1975-02-20', 'F', '16391459405');
+
+
+INSERT INTO VACINACAO.VACINA (id_Pesquisador, nome, pais_Origem, estagio_Da_Pesquisa, dataInicioDaPesquisa) values
+(5, 'CoronaVac', 'China', 1, '2022-11-21'),
+(4, 'AstraZeneca', 'Reino Unido', 2, '2022-11-22'),
+(2, 'Pfizer', 'Estados Unidos', 3, '2022-11-30'),
+(6, 'Moderna', 'Estados Unidos', 1, '2023-01-01'),
+(7, 'Johnson & Johnson', 'Estados Unidos', 2, '2023-01-02'),
+(8, 'Sputnik V', 'Rússia', 3, '2023-01-03');
+
+INSERT INTO VACINACAO.APLICACAO_VACINA (id_Pessoa, id_Vacina, dataAplicacao, avaliacaoDaReacao) VALUES 
+(1, 1, '2022-11-21', 1),
+(2, 2, '2022-11-22', 2),
+(3, 3, '2022-11-30', 5),
+(6, 4, '2023-01-01', 3),
+(7, 5, '2023-01-02', 4),
+(8, 6, '2023-01-03', 5);
+
+
